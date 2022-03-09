824d069df79bfc9fd26d271d8387cd711f2867e1
unknown <jaiesden@gmail.com>
2022-03-09 12:17:34 +0530

    verify

---
+++ src/test/java/com/test/git/GitCode.java
@@ -0,0 +1,16 @@
+package com.test.git;
+
+public class GitCode {
+	public void method11() {
+		System.out.println("method11");
+
+	}
+	public void method12() {
+	System.out.println("method12");
+
+	}
+	public void method13() {
+	System.out.println("method12");
+
+}
+}
